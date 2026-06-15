cddb8d1ab2573c21478eaaf96d731e9fdd0edbd5
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-16 01:06:42 +0530

    feat: add User entity mapped to users table

---
+++ src/main/java/com/icet/carrental/model/User.java
@@ -0,0 +1,24 @@
+package com.icet.carrental.model;
+
+import com.icet.carrental.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long          id;
+    private String        name;
+    private String        email;
+    private String        password;
+    private String        phone;
+    private UserRole      role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
