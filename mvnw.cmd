7127779c9ec0c39b22a4dcfcf68f064ecab4a059
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-17 12:48:30 +0530

    build: add Maven wrapper for consistent project builds

---
+++ mvnw.cmd
@@ -0,0 +1,70 @@
+@REM ----------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM    https://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM ----------------------------------------------------------------------------
+
+@REM ----------------------------------------------------------------------------
+@REM Apache Maven Wrapper startup batch script, version 3.3.2
+@REM ----------------------------------------------------------------------------
+
+@IF "%__MVNW_ARG0_NAME__%"=="" (SET __MVNW_ARG0_NAME__=%~nx0)
+@SET __MVNW_CMD__=
+@IF NOT "%MAVEN_BATCH_ECHO%" == ""  ECHO %MAVEN_BATCH_ECHO%
+
+@REM set %HOME% to equivalent of $HOME
+IF "%HOME%"=="" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
+
+@REM Execute a user defined script before this one
+IF NOT "%MAVEN_SKIP_RC%"=="" (
+  IF EXIST "%USERPROFILE%\mavenrc_pre.bat" call "%USERPROFILE%\mavenrc_pre.bat" %*
+)
+
+@setlocal
+
+SET MAVEN_PROJECTBASEDIR=%~dp0
+IF NOT "%MAVEN_PROJECTBASEDIR:~-1%"=="" SET MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR:~0,-1%
+SET WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
+SET WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+
+FOR /F "usebackq tokens=1,2 delims==" %%A IN ("%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties") DO (
+    IF "%%A"=="wrapperUrl" SET WRAPPER_URL=%%B
+)
+
+IF NOT EXIST %WRAPPER_JAR% (
+    IF DEFINED WRAPPER_URL (
+        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%WRAPPER_URL%' -OutFile '%WRAPPER_JAR%'}"
+    )
+)
+
+@REM Provide a "standardized" way to retrieve the CLI args that will work
+@REM with both Windows and non-Windows executions.
+set MAVEN_CMD_LINE_ARGS=%*
+
+"%MAVEN_JAVA_EXE%" %JVM_CONFIG_MAVEN_PROPS% ^
+  %MAVEN_OPTS% ^
+  %MAVEN_DEBUG_OPTS% ^
+  -classpath %WRAPPER_JAR% ^
+  "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" ^
+  %WRAPPER_LAUNCHER% %*
+
+@endlocal & set ERROR_CODE=%ERRORLEVEL%
+
+IF NOT "%MAVEN_SKIP_RC%"=="" (
+  IF EXIST "%USERPROFILE%\mavenrc_post.bat" call "%USERPROFILE%\mavenrc_post.bat"
+)
+
+exit /b %ERROR_CODE%
