eef509314d871cfe7fdf68c54f43f6a1ad993f91
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-17 12:48:33 +0530

    test: add Spring Boot application context load test

---
+++ src/test/java/com/icet/carrental/CarRentalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.icet.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
