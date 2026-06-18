3e10b4912cd9fab0690322537ffbf0f29328486f
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-18 11:05:14 +0530

    feat: add InvalidFileException and StorageException

---
+++ src/main/java/com/icet/carrental/exception/InvalidFileException.java
@@ -0,0 +1,8 @@
+package com.icet.carrental.exception;
+
+public class InvalidFileException extends RuntimeException {
+
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
