7e8d62dab22754a9bac1df969c67d30f3d4d51ae
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-20 23:59:49 +0530

    refactor: add timeouts to RestClient HTTP factory

--- src/main/java/com/icet/carrental/config/RestClientConfig.java
+++ src/main/java/com/icet/carrental/config/RestClientConfig.java
@@ -2,13 +2,22 @@ package com.icet.carrental.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(10));
+        requestFactory.setReadTimeout(Duration.ofSeconds(30));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
     }
 }
