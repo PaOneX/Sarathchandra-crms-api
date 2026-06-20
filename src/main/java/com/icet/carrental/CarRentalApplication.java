2a5a8b8b948d116442abd8fd635f818da7faad88
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-20 23:59:49 +0530

    refactor: centralize configuration properties registration

--- src/main/java/com/icet/carrental/CarRentalApplication.java
+++ src/main/java/com/icet/carrental/CarRentalApplication.java
@@ -1,9 +1,22 @@
 package com.icet.carrental;
 
+import com.icet.carrental.config.AdminSeedProperties;
+import com.icet.carrental.config.CorsProperties;
+import com.icet.carrental.config.StripeProperties;
+import com.icet.carrental.config.SupabaseProperties;
+import com.icet.carrental.config.WhatsAppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        AdminSeedProperties.class,
+        CorsProperties.class,
+        StripeProperties.class,
+        SupabaseProperties.class,
+        WhatsAppProperties.class
+})
 public class CarRentalApplication {
 
     public static void main(String[] args) {
