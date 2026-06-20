56586171c87a12f87a41f5ccbdfc7fac6b4241da
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-20 23:59:49 +0530

    feat: add CorsProperties for configurable allowed origins

---
+++ src/main/java/com/icet/carrental/config/CorsProperties.java
@@ -0,0 +1,13 @@
+package com.icet.carrental.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.cors")
+public class CorsProperties {
+
+    private String allowedOrigins = "";
+}
